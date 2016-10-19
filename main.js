e7b1517a767151e7557de33a20097d9f141acc74
jonick makau <r0591009@student.thomasmore.be>
2016-10-19 13:51:55 +0200

    reponse te krijgen

--- main.js
+++ main.js
@@ -23,3 +23,12 @@ app.get("/uitkomst", function(response)
 //om json terug te sturen
   {response.send(uitkomst);
 });
+
+//reponse te krijgen 
+app.post("/", function(request, response){
+    var berekening = request.body.display;
+    var uitrekenen = eval(request.body.display);
+    uitkomst.push({"berekening": berekening, "uitkomst": uitrekenen});
+    console.log(uitrekenen);
+    response.status(203).send(uitkomst);
+});
