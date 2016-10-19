7a58033ca624a4491201ccdab88ad71a749452bc
jonick makau <r0591009@student.thomasmore.be>
2016-10-19 13:41:31 +0200

    app aanmaken voor url

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 //varibel maken
 var express = require('express');
 var bodyparser = require('body-parser');
-var app = express();
+var app = express
+
+//app aanmaken voor url
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
